7f5c49e8640c8910a815c2e1f7a2c2244d67a1ac
PCWIN10 <PCWIN10@mail.ru>
2020-01-08 21:53:47 +0500

    Fixed #3

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ func main() {
 }
 
 func bonus(amount [] int) int {
-	const boundPurchanse = 10_000
+	const boundForBonus = 10_000
 	const percent = 5
 	amountBonus := 0
 	for _, amount := range amount {
-		if amount > boundPurchanse {
-			amountBonus += (amount - boundPurchanse) * percent / 100
+		if amount > boundForBonus {
+			amountBonus += (amount - boundForBonus) * percent / 100
 		}
 	}
 	return amountBonus
